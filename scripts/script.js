1fbf4975f1e47eb04f68d0316fd9b1ea3c0d1746
Bruno Pasqual <brunoh.pasqual0@gmail.com>
2022-12-28 21:17:15 -0300

    validação input ok

--- scripts/script.js
+++ scripts/script.js
@@ -3,21 +3,72 @@
 const inputNome = document.getElementById('input_nome');
 const inputEmail = document.getElementById('input_email');
 const inputCelular = document.getElementById('input_numero');
-const mensagensErro = document.querySelectorAll('mensagem_erro');
+const mensagensErro = document.querySelectorAll('.mensagem_erro');
 
 //! Funções -------------------------------------------------------------------
 
-function checaNome() {
-  if (apenasLetras(inputNome.value)) {
-    console.log('só letras');
-  } else {
-    inputNome.style.outline = '2px ';
-  }
-}
-
+//todo ---------------------------------------- Expressões regulares
 //Regex para testar validade de um email
-let regex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+function emailEhValido(email) {
+  return /^[\w+.]+@\w+\.\w{3,}(?:\.\w{2})?$/.test(email);
+}
 
 function apenasLetras(str) {
   return /^[A-Za-z\s]*$/.test(str);
+}
+//todo ---------------------------------------- Fim expressões regulares
+
+function checaNome() {
+  //todo Função que irá fazer algumas verificações no input do nome e alterará a cor do outline e também a mensagem de erro.
+
+  if (inputNome.value === '') {
+    inputNome.style.outline = '1px solid #ee374a ';
+    mensagensErro[0].style.display = 'block';
+    mensagensErro[0].textContent = 'Não pode ficar vazio';
+  } else if (!apenasLetras(inputNome.value)) {
+    inputNome.style.outline = '1px solid #ee374a ';
+    mensagensErro[0].style.display = 'block';
+    mensagensErro[0].textContent = 'Apenas letras';
+  } else {
+    inputNome.style.outline = '1px solid green ';
+    mensagensErro[0].style.display = 'none';
+  }
+}
+
+function checarEmail() {
+  //todo Função que irá fazer algumas verificações de email, não é 100%, essa verificação não pega algumas coisas.
+
+  if (inputEmail.value === '') {
+    inputEmail.style.outline = '1px solid #ee374a ';
+    mensagensErro[1].style.display = 'block';
+    mensagensErro[1].textContent = 'Não pode ficar vazio';
+  } else if (!emailEhValido(inputEmail.value)) {
+    inputEmail.style.outline = '1px solid #ee374a ';
+    mensagensErro[1].style.display = 'block';
+    mensagensErro[1].textContent = 'Email inválido';
+  } else {
+    inputEmail.style.outline = '1px solid green ';
+    mensagensErro[1].style.display = 'none';
+  }
+}
+
+function checarNumero() {
+  //todo Função que irá fazer a verificação do input do número de celular, funciona de forma similar as anteriores, de forma que a mensagem de erro e o outline do input são alterados de acordo.
+
+  if (inputCelular.value === '') {
+    inputCelular.style.outline = '1px solid #ee374a ';
+    mensagensErro[2].style.display = 'block';
+    mensagensErro[2].textContent = 'Não pode ficar vazio';
+  } else if (inputCelular.value.length < 12) {
+    inputCelular.style.outline = '1px solid #ee374a ';
+    mensagensErro[2].style.display = 'block';
+    mensagensErro[2].textContent = 'Não esquece do DDD ex. 015';
+  } else if (inputCelular.value.length > 12) {
+    inputCelular.style.outline = '1px solid #ee374a ';
+    mensagensErro[2].style.display = 'block';
+    mensagensErro[2].textContent = 'Número inválido';
+  } else {
+    inputCelular.style.outline = '1px solid green';
+    mensagensErro[2].style.display = 'none';
+  }
 }
